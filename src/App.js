f6264d3986a8beed3bbf10a756278fd52721f746
Gortenzik <gortenzik@yandex.ru>
2022-09-30 18:30:10 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
+import Link from './Link';
+
 
 function App() {
+
+const [move, setMove] = useState(data);
+// console.log(data);
+
+const removeFilm = (id) => {
+  // console.log(id);
+    let newFilm = move.filter(filmName => filmName.id !== id);
+    console.log(newFilm);
+    setMove(newFilm);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+        <div className='container'>
+            <Link/>
+        </div>
+
+        <div className='container'>
+            <h2>Осталось посмотреть {move.length} фильмов:</h2>
+        </div>
+
+    {move.map((mean => {
+        const {id, nameCinema} = mean;
+  
+
+        return (
+            <div key={id}>
+
+    <div className='row'>
+        <div className='row'>
+            <p>{nameCinema}</p>
+        </div>
+
+        <div className='row'>
+            <button className='btn' onClick={() => removeFilm(id)}>Удалить</button>
+        </div>
+    </div>
+
+            </div>
+        )
+
+        }))} 
+        
+
+            <div className='container'>
+              <button className='btn' onClick={() => {setMove([])}}>Удалить все</button>
+            </div>
+
     </div>
   );
 }
